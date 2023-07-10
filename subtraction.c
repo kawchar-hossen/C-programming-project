d4dab81397b0ebd3169ce162e08314b4d83b8259
kawchar-hossen <138645451+kawchar-hossen@users.noreply.github.com>
2023-07-10 15:44:37 +0600

    Update subtraction.c

---
+++ subtraction.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+  int n1,n2,sub;
+  printf("Enter first number : ");
+  scanf("%d",&n1);
+  printf("Enter second number: ");
+  scanf("%d",&n2);
+  sub = n1-n2;
+  printf("Result is : %d",sub);
+  return 0;
+}
